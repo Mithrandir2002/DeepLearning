66055097db0718e55063547ab3cf301f6788bcc8
Mithrandir2002 <thanhthuy.pthn@gmail.com>
2024-09-09 16:51:11 +0700

    Visualize loss function when yi = 0 and yi = 1

---
+++ ex5.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Predicted probabilities
+y_hat = np.linspace(0.01, 0.99, 100)
+
+# True label y_i = 0
+y_i = 0
+
+# Loss function
+loss = -(y_i * np.log(y_hat) + (1 - y_i) * np.log(1 - y_hat))
+
+# Plotting
+plt.plot(y_hat, loss, label=r'Loss when $y_i = 0$')
+plt.xlabel(r'$\hat{y}$')
+plt.ylabel('Loss')
+plt.title('Binary Cross-Entropy Loss Function')
+plt.grid(True)
+plt.legend()
+plt.show()
